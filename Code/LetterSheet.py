e82dfcd8e7b2e170927d4c34318754a0ee558caf
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2021-06-07 11:16:25 -0400

    adding updated lettersheet

--- Code/LetterSheet.py
+++ Code/LetterSheet.py
@@ -1,7 +1,7 @@
-
+#UNFINISHED
 
 class Player:
-    def person(x,y,s):
+    def person(x,y,s):      #will create a small humanoid player
         s[x,y]=5
         s[x+1,y]=0
         s[x+2,y]=5
@@ -16,164 +16,175 @@ class Player:
         s[x+1,y+4]=5
         s[x+2,y+4]=0
 
-class Letter:
+class Letter:          #creates 3x5 letters followed by a space from individual 1x5 slices 
 
     def A(x,y,s):
-        s[x+1,y]=1
-        s[x+3,y]=24
+        s[x+1,y]=129
+        s[x+3,y]=145
 
     def B(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=2
-        s[x+2,y]=3
-        s[x+3,y]=105
+        s[x+1,y]=130
+        s[x+2,y]=131
+        s[x+3,y]=145
 
     def C(x,y,s):
-        s[x,y]=6
-        s[x+1,y]=4
-        s[x+2,y]=4
-        s[x+3,y]=105
+        s[x+1,y]=132
+        s[x+2,y]=132
+        s[x+3,y]=145
 
     def D(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=7
-        s[x+2,y]=11
-        s[x+3,y]=105
+        s[x+1,y]=132
+        s[x+2,y]=133
+        s[x+3,y]=145
 
     def E(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=4
-        s[x+2,y]=7
-        s[x+3,y]=105
-
+        s[x+1,y]=130
+        s[x+2,y]=132
+        s[x+3,y]=145
 
     def F(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=1
-        s[x+2,y]=17
-        s[x+3,y]=105
+        s[x+1,y]=129
+        s[x+2,y]=134
+        s[x+3,y]=145
 
     def G(x,y,s):
-        s[x,y]=18
-        s[x+1,y]=4
-        s[x+2,y]=0
-        s[x+3,y]=105
+        s[x,y]=135
+        s[x+1,y]=130
+        s[x+3,y]=145
 
     def H(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=22
-        s[x+2,y]=0
-        s[x+3,y]=105
+        s[x+1,y]=136
+        s[x+3,y]=145
 
     def I(x,y,s):
-        s[x,y]=7
-        s[x+1,y]=0
-        s[x+2,y]=7
-        s[x+3,y]=105
+        s[x,y]=132
+        s[x+2,y]=132
+        s[x+3,y]=145
 
     def J(x,y,s):
-        s[x,y]=7
-        s[x+1,y]=0
-        s[x+2,y]=17
-        s[x+3,y]=105
+        s[x,y]=132
+        s[x+2,y]=134
+        s[x+3,y]=145
 
     def K(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=22
-        s[x+2,y]=32
-        s[x+3,y]=105
-
+        s[x+1,y]=136
+        s[x+2,y]=137
+        s[x+3,y]=145
 
     def L(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=34
-        s[x+2,y]=34
-        s[x+3,y]=105
+        s[x+1,y]=138
+        s[x+2,y]=138
+        s[x+3,y]=145
 
     def M(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=47
-        s[x+2,y]=39
-        s[x+3,y]=47
-        s[x+4,y]=39
-        s[x+5,y]=105
+        s[x+1,y]=140
+        s[x+2,y]=139
+        s[x+3,y]=140
+        s[x+5,y]=145
 
     def N(x,y,s):
-        s[x,y]=39
-        s[x+1,y]=22
-        s[x+2,y]=39
-        s[x+3,y]=105
+        s[x,y]=139
+        s[x+1,y]=136
+        s[x+2,y]=139
+        s[x+3,y]=145
 
     def O(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=7
-        s[x+2,y]=0
-        s[x+3,y]=105
+        s[x+1,y]=132
+        s[x+3,y]=145
 
     def P(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=1
-        s[x+2,y]=47
-        s[x+3,y]=105
+        s[x+1,y]=129
+        s[x+2,y]=140
+        s[x+3,y]=145
 
     def Q(x,y,s):
-        s[x,y]=48
-        s[x+1,y]=49
-        s[x+2,y]=0
-        s[x+3,y]=105
+        s[x,y]=141
+        s[x+1,y]=142
+        s[x+3,y]=145
 
     def R(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=52
-        s[x+2,y]=18
-        s[x+3,y]=105
+        s[x+1,y]=143
+        s[x+2,y]=135
+        s[x+3,y]=145
 
     def S(x,y,s):
-        s[x,y]=18
-        s[x+1,y]=4
-        s[x+2,y]=56
-        s[x+3,y]=105
+        s[x,y]=135
+        s[x+1,y]=130
+        s[x+2,y]=143
+        s[x+3,y]=145
 
     def T(x,y,s):
-        s[x,y]=57
-        s[x+1,y]=58
-        s[x+2,y]=59
-        s[x+3,y]=105
+        s[x,y]=134
+        s[x+2,y]=134
+        s[x+3,y]=145
 
     def U(x,y,s):
-        s[x,y]=0
-        s[x+1,y]=34
-        s[x+2,y]=0
-        s[x+3,y]=105
+        s[x+1,y]=138
+        s[x+3,y]=145
 
     def V(x,y,s):
-        s[x,y]=48
-        s[x+1,y]=34
-        s[x+2,y]=48
-        s[x+3,y]=105
+        s[x,y]=141
+        s[x+1,y]=138
+        s[x+2,y]=141
+        s[x+3,y]=145
 
     def W(x,y,s):
-        s[x,y]=48
-        s[x+1,y]=39
-        s[x+2,y]=11
-        s[x+3,y]=39
-        s[x+4,y]=48
-        s[x+5,y]=105
+        s[x,y]=141
+        s[x+1,y]=139
+        s[x+2,y]=140
+        s[x+3,y]=139
+        s[x+4,y]=141
+        s[x+5,y]=145
 
     def X(x,y,s):
-        s[x,y]=32
-        s[x+1,y]=22
-        s[x+2,y]=32
-        s[x+3,y]=105
+        s[x,y]=137
+        s[x+1,y]=136
+        s[x+2,y]=137
+        s[x+3,y]=145
 
     def Y(x,y,s):
-        s[x,y]=47
-        s[x+1,y]=39
-        s[x+2,y]=47
-        s[x+3,y]=105
+        s[x,y]=140
+        s[x+1,y]=139
+        s[x+2,y]=140
+        s[x+3,y]=145
 
     def Z(x,y,s):
-        s[x,y]=75
-        s[x+1,y]=4
-        s[x+2,y]=77
-        s[x+3,y]=105
+        s[x,y]=273
+        s[x+1,y]=130
+        s[x+2,y]=274
+        s[x+3,y]=145
+
+    def ONE(x,y,s):
+        s[x,y]=132
+        s[x+2,y]=138
+        s[x+3,y]=145
+
+    #def TWO(x,y,s):        #ran into memory errors at the end with some of these numbers
+      #  s[x,y]=143
+     #   s[x+1,y]=130
+     #   s[x+2,y]=135
+      #  s[x+3,y]=145
+
+    def THREE(x,y,s):
+        s[x,y]=130
+        s[x+1,y]=130
+        s[x+3,y]=145
+
+    def FOUR(x,y,s):
+        s[x,y]=140
+        s[x+1,y]=136
+        s[x+3,y]=145
+
+    #def SIX(x,y,s):
+       # s[x+1,y]=130
+       # s[x+2,y]=139
+       # s[x+3,y]=145
+
+   # def SEVEN(x,y,s):
+    #    s[x,y]=134
+    #    s[x+1,y]=134
+      #  s[x+3,y]=145
+
+    def EIGHT(x,y,s):
+        s[x+1,y]=130
+        s[x+3,y]=145
